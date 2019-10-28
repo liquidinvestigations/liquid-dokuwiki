b7e93ab98610565274e0e0af64a0a12a07b8fbaa
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-10-28 16:24:38 +0200

    interpolate dokuwiki vars on one line

--- conf/local.php
+++ conf/local.php
@@ -1,6 +1,5 @@
 <?php
-$conf['title']  = '<a style="display:inline-block;margin-left:10px;" href="' . $_SERVER["LIQUID_CORE_URL"] . '">&#8594; ' . $_SERVER["LIQUID_TITLE"] . '</a><script src="https://hypothesis.' . $_SERVER["LIQUID_DOMAIN"] . '/embed.js"></script>';
-
+$conf['title']  = "<a style=\"display:inline-block;margin-left:10px;\" href=\"{$_SERVER['LIQUID_CORE_URL']}\">&#8594; {$_SERVER["LIQUID_TITLE"]}</a><script src=\"https://hypothesis.{$_SERVER["LIQUID_DOMAIN"]}/embed.js\"></script>";
 $conf['lang'] = 'en';
 $conf['template'] = 'liquid';
 $conf['license'] = '0';
@@ -9,4 +8,4 @@ $conf['superuser'] = '@admin';
 $conf['disableactions'] = 'register';
 $conf['authtype'] = 'liquid';
 $conf['defaultgroup'] = 'admin,user';
-$conf['baseurl'] = $_SERVER["LIQUID_HTTP_PROTOCOL"] . '://dokuwiki.' . $_SERVER["LIQUID_DOMAIN"];
+$conf['baseurl'] = "{$_SERVER["LIQUID_HTTP_PROTOCOL"]}://dokuwiki.{$_SERVER["LIQUID_DOMAIN"]}";
