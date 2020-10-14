ca3cae35f4489a79db1dc5dfc39582ba5c327e8c
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-10-14 17:56:45 +0300

    get profile data on external auth

--- conf/local.protected.php
+++ conf/local.protected.php
@@ -1,5 +1,5 @@
 <?php
-$conf['title']  = "<a style=\"display:inline-block;margin-left:10px;\" href=\"{$_SERVER['LIQUID_CORE_URL']}\">&#8594; {$_SERVER["LIQUID_TITLE"]}</a><script src=\"https://hypothesis.{$_SERVER["LIQUID_DOMAIN"]}/embed.js\"></script>";
+$conf['title']  = "<a style=\"display:inline-block;margin-left:10px;\" href=\"{$_SERVER['LIQUID_CORE_URL']}\">&#8594; {$_SERVER["LIQUID_TITLE"]}</a><script src=\"{$_SERVER["LIQUID_HTTP_PROTOCOL"]}://hypothesis.{$_SERVER["LIQUID_DOMAIN"]}/embed.js\"></script>";
 $conf['lang'] = 'en';
 $conf['template'] = 'liquid';
 $conf['license'] = '0';
--- plugins/liquid/auth.php
+++ plugins/liquid/auth.php
@@ -8,18 +8,37 @@ class auth_plugin_liquid extends DokuWiki_Auth_Plugin {
     $this->cando['external'] = true;
     $this->success = true;
   }
+
+  function profile($access_token) {
+    // Use the following to get the user profile data from the oauth2 provider.
+    // https://www.php.net/manual/en/function.file-get-contents.php
+    // Imitates the hoover-search contrib.auth.oauth2 implementation for the client:
+    // https://github.com/liquidinvestigations/hoover-search/blob/7cc4751996091e5e88708681b87f57f45c7d34a4/hoover/contrib/oauth2/views.py#L53
+    // We get the access token from oauth2-proxy X-Forwarded-Access-Token header: https://oauth2-proxy.github.io/oauth2-proxy/configuration
+    $opts = array(
+      'http'=>array(
+        'method'=>"GET",
+        'header'=>"Authorization: Bearer {$access_token}\r\n"
+      )
+    );
+    $context = stream_context_create($opts);
+    $data = file_get_contents(getenv('LIQUID_CORE_PROFILE_URL'), false, $context);
+    return json_decode($data);
+  }
+
   function trustExternal($user, $pass, $sticky = false) {
     global $USERINFO;
-    if (isset($_SERVER['HTTP_X_FORWARDED_USER'])) {
-      $userid = $_SERVER['HTTP_X_FORWARDED_USER'];
+
+    if (isset($_SERVER['X_FORWARDED_ACCESS_TOKEN'])) {
+      $token = $_SERVER['X_FORWARDED_ACCESS_TOKEN'];
+      $profile = $this->profile($token);
+      $userid = $profile['id'];
+
       $USERINFO['user'] = $userid;
-      $USERINFO['mail'] = $_SERVER['HTTP_X_FORWARDED_USER_EMAIL'];
-      $USERINFO['name'] = $_SERVER['HTTP_X_FORWARDED_USER_FULL_NAME'];
-      if ($_SERVER['HTTP_X_FORWARDED_USER_ADMIN'] == 'true') {
-        $USERINFO['grps'] = array('admin', 'user');
-      } else {
-        $USERINFO['grps'] = array('user');
-      }
+      $USERINFO['mail'] = $profile['email'];
+      $USERINFO['name'] = $profile['name'];
+      $USERINFO['grps'] = $profile['roles'];
+
       $_SERVER['REMOTE_USER'] = $userid;
       $_SESSION[DOKU_COOKIE]['auth']['user'] = $userid;
       $_SESSION[DOKU_COOKIE]['auth']['info'] = $USERINFO;
